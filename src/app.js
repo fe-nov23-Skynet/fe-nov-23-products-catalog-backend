f5e0b4a0bfaaa74ffe7e6034bd154dc7cf80d541
Yaroslav Samoilenko <yasik2255@live.com>
2024-03-10 17:39:18 +0200

    realized chatGPT usage in beta mode

--- src/app.js
+++ src/app.js
@@ -14,21 +14,77 @@ const io = new Server(server, {
   }
 });
 
+const adminImgURL = '/img/admin_avatar.avif';
+const userImgURL = '/img/user_avatar.avif';
+
 const PORT = 4000; /* process.env.PORT || 4000; */
 const HOST = '0.0.0.0';
 server.listen(PORT, HOST, () => {
   console.log(`listening on *:${PORT}`);
 });
 
-
+const connections = [];
 let rooms = [];
+let adminsList = [];
+
+function getUserBySocketIssued(issued) {
+  return connections.find(c => c.handshake.issued === Number(issued)) || null;
+}
+
+function sendRoomsToAdmins() {
+  adminsList.forEach((adminId) => {
+    getUserBySocketIssued(adminId).emit('server:rooms', rooms);
+  });
+}
+
+async function createRoom(socket) {
+  const { sendMessage, listenAIAnswers } = await createAIChat();
+
+
+
+  const room = {
+    id: (socket.handshake.issued).toString(),
+    members: [socket.handshake.issued.toString()],
+    messages: [],
+    sendMessageToAI: sendMessage,
+   };
+
+  function sendMessageToRoom(room) {
+   return (messagesList) => {
+    messagesList.forEach(message => {
+      if (!room.messages.some(m => m.body === message.body)){
+        room.messages.push(message);
+        io.to(room.id).emit('server:msg', message)
+      }
+    });
+
+  };
+  }
+
+  listenAIAnswers(sendMessageToRoom(room));
+
+  if (rooms.every(r => r.id !== room.id) ) {
+    rooms.push(room);
+  }
+  socket.join(room.id);
+
+  sendRoomsToAdmins();
+
+  return room;
+}
 
 io.sockets.on('connection', (socket) => {
   console.log('a user connected');
+  connections.push(socket);
 
   socket.on('disconnect', () => {
     console.log('user disconnected now');
     rooms = rooms.filter(r => r.id !== String(socket.handshake.issued));
+    sendRoomsToAdmins();
+
+    if (adminsList.includes(String(socket.handshake.issued)) ) {
+      adminsList = adminsList.filter(a => a !== String(socket.handshake.issued));
+    }
   });
 
   socket.on('user:msg', (message) => {
@@ -44,36 +100,29 @@ io.sockets.on('connection', (socket) => {
       authorName: message.authorName,
       body: message.body,
     };
-    room.messages.push(message);
-    io.in(room.id).emit('server:msg', message1);
+    room.sendMessageToAI(message.body);
+    /* room.messages.push(message);
+    io.in(room.id).emit('server:msg', message1); */
   });
 
-  socket.on('user:needHelp', (data) => {
+  socket.on('user:needHelp', async (data) => {
     console.log(`User need help.`);
 
-    const room = {
-      id: (socket.handshake.issued).toString(),
-      members: [socket.handshake.issued.toString()],
-      messages: [],
-    };
-
-    if (rooms.every(r => r.id !== room.id) ) {
-      rooms.push(room);
-    }
-    socket.join(room.id);
+    const room = await createRoom(socket);
     const responce = {
       roomId: room.id,
       authorId: 0,
       authorName: 'ADMIN',
       avatarURL: '/img/admin_avatar.avif',
-      body: 'Hello! How we can help you?',
+      body: 'Hello! AI helper will help you soon. Please wait.',
     };
     io.to(room.id).emit('server:msg', responce);
     console.log('room created:', room.id);
-    console.log('rooms:', rooms);
   });
 
   socket.on('admin:rooms', () => {
+    const socketIssue = String(socket.handshake.issued);
+    adminsList.push(socketIssue);
     console.log('admin:rooms');
     socket.emit('server:rooms', rooms);
   });
@@ -96,6 +145,7 @@ io.sockets.on('connection', (socket) => {
         body: 'Support team member joined the chat.',
       };
       io.to(room.id).emit('server:msg', responce);
+      sendRoomsToAdmins();
       console.log('admin:joinRoom', data);
     }
   });
@@ -116,6 +166,119 @@ io.sockets.on('connection', (socket) => {
     io.in(room.id).emit('server:msg', message1);
   });
 });
+
+
+const openai = new OpenAI({
+  apiKey: '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',
+});
+
+async function createAIChat() {
+  const assistant = await openai.beta.assistants.retrieve('asst_gc3HWkC5sYga6z1FOjzzjeT8');
+  const thread = await openai.beta.threads.create();
+
+  async function sendMessage(text) {
+    if (assistant && thread) {
+      const message = await openai.beta.threads.messages.create(
+        thread.id,
+        {
+          role: 'user',
+          content: text,
+        },
+      );
+      const run = await openai.beta.threads.runs.create(
+        thread.id,
+        {
+          assistant_id: assistant.id,
+        },
+      );
+    }
+  }
+  function listenAIAnswers(sendAIAnswer) {
+    const listener = setInterval(async () => {
+      if (assistant && thread) {
+        const messagesArray = await openai.beta.threads.messages.list(
+          thread.id,
+        );
+        const newMessages = messagesArray.data.map(msg => {
+          if ('text' in msg.content[0]) {
+            return {
+              authorId: msg.role === 'assistant' ? 1 : 2,
+              authorName: 'Alice',
+              body: msg.content[0].text.value,
+              avatarURL: msg.role === 'assistant' ? adminImgURL : userImgURL,
+            };
+          }
+          return null;
+        });
+        sendAIAnswer(newMessages);
+      }
+    }, 2000);
+  }
+
+  return {
+    sendMessage,
+    listenAIAnswers,
+  };
+}
+
+
+// chatGPT
+/*   useEffect(() => {
+    const fetchData = async () => {
+      const assistant1 = await openai.beta.assistants.retrieve('asst_gc3HWkC5sYga6z1FOjzzjeT8');
+      setAssistant(assistant1);
+      const thread1 = await openai.beta.threads.create();
+      setThread(thread1);
+    };
+
+    fetchData();
+  }, []);
+
+  async function sendMessage() {
+    setMessageText('');
+
+    if (assistant && thread) {
+      const message = await openai.beta.threads.messages.create(
+        thread.id,
+        {
+          role: 'user',
+          content: messageText,
+        },
+      );
+      const run = await openai.beta.threads.runs.create(
+        thread.id,
+        {
+          assistant_id: assistant.id,
+        },
+      );
+    }
+  }
+
+  useEffect(() => {
+    setInterval(async () => {
+      if (assistant && thread) {
+        const messages3 = await openai.beta.threads.messages.list(
+          thread.id,
+        );
+        console.log(messages3);
+        const newMessages = messages3.data.map(msg => {
+          if ('text' in msg.content[0]) {
+            return {
+              authorId: msg.role === 'assistant' ? 1 : 2,
+              authorName: 'Alice',
+              body: msg.content[0].text.value,
+              avatarURL: msg.role === 'assistant' ? adminImgURL : userImgURL,
+            };
+          }
+          return null;
+        });
+        setMessages(newMessages.filter(Boolean).reverse() as Message[]);
+      }
+    }, 2000);
+  }, [assistant, thread]); */
+
+
+
 
 
 /* const openai = new OpenAI({
